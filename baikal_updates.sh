1f9a60bd87d9efb802882697a6e80beb58d6373a
Florian Latzel <florian.latzel@gmail.com>
2022-07-08 20:15:37 +0200

    Added working bash script.

---
+++ baikal_updates.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# baikal_updates.sh
+
+# Check for available updates of the Baïkal CalDAV and CardDAV server
+# by comparing current version with the version of the latest release from Github 
+
+# Usage: baikal_updates.sh /path/to/baikal
+
+set -e
+set -o nounset 
+
+baikal=$1 && test -d $baikal || { echo "$baikal is no directory."; exit; }
+version=$(grep 'configured_version' ${baikal}/config/baikal.yaml | tr -d ' ' | cut -d ':'  -f 2)
+current_version=${version:?}
+latest_version=$(curl -s https://api.github.com/repos/sabre-io/Baikal/releases/latest | jq -r '.name')
+
+if [ "$current_version" != "$latest_version" ] ; then 
+   latest_body=$(curl -s https://api.github.com/repos/sabre-io/Baikal/releases/latest | jq -r '.body')
+   echo -e "Baïkal update available:" 
+   echo " - Running $current_version -> $latest_version is available"
+   echo -e "Release notes:\n $latest_body"
+fi
